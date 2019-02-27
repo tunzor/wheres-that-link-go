1d7a540fa4100c4930cc3637ad4b04e779403109
tunzor <russo555@gmail.com>
2019-02-26 20:49:25 -0500

    Implmented popup background colour changing based on whether URL is on same domain or is using a URL shortener.

--- hover.js
+++ hover.js
@@ -1,9 +1,10 @@
-//TODO: expand bit.ly and other shortened links
-//TODO: changing background colour based on internal/external link
-
 var enabled = false;
 var currentHoverLink = null;
 var currentMousePos = { x: -1, y: -1 };
+
+var pagePath = window.location.pathname;
+var fullUrl = window.location.href;
+var domain = window.location.origin;
 
 // Gets current cursor position for drawing popup
 $(document).mousemove(function(event) {
@@ -42,11 +43,22 @@ $(document).ready(function(){
 function getPopup() {
 	return "<div id='wtlg_popup' style='display: none; z-index: 1000; "
 		+ "background-color: #e3e3e3; position: absolute;"
-		+ "border: 2px solid green; left: 0px; top: 0px; font-size: 16pt;"
+		+ "border: 2px solid black; left: 0px; top: 0px; font-size: 16pt;"
 		+ "'</div>";
 }
 
 function updatePopup(url, left, top) {
+	// Check if URL is on same domain
+	if(url.includes(domain) || url.charAt(0) == "/" ) {
+		$("#wtlg_popup").css("background-color", "#4fe27b");
+	// Check if URL is shortened using bit.ly, goo.gl, or t.co
+	} else if(url.includes("http://bit.ly") || url.includes("https://bit.ly")
+			|| url.includes("http://goo.gl") || url.includes("https://goo.gl")
+			|| url.includes("http://t.co") || url.includes("https://t.co")) {
+		$("#wtlg_popup").css("background-color", "#f7f742");
+	} else {
+		$("#wtlg_popup").css("background-color", "#d8d8d8");
+	}
 	$("#wtlg_popup").css("left",left);
 	$("#wtlg_popup").css("top",top);
 	$("#wtlg_popup").html(url);
